dae906c5ed7df1b3149ecfa6c1ef1374747cc2aa
SzGajdzica <sz.gajdzica@labmatic.com>
2020-06-03 17:07:26 +0200

    minor changes

--- src/main/java/pl/polsl/courier/system/models/Client.java
+++ src/main/java/pl/polsl/courier/system/models/Client.java
@@ -4,7 +4,10 @@ import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 import java.util.LinkedList;
 import java.util.List;
@@ -31,7 +34,7 @@ public class Client extends IdEntity {
     @NotNull
     private String phoneNumber;
 
-    @OneToMany(mappedBy = "client", cascade = CascadeType.DETACH)
+    @OneToMany(mappedBy = "client")
     @NotNull
     private List<Package> packages = new LinkedList<>();
 
--- src/main/java/pl/polsl/courier/system/models/Package.java
+++ src/main/java/pl/polsl/courier/system/models/Package.java
@@ -4,7 +4,10 @@ import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 import java.util.Date;
 
@@ -23,7 +26,7 @@ public class Package extends IdEntity {
     @NotNull
     private Client client;
 
-    @ManyToOne(cascade = CascadeType.REMOVE)
+    @ManyToOne()
     private Car car;
 
     @ManyToOne(optional = false)
